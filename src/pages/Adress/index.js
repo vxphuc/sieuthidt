1c2f4a526120f304cc733dad134e59ea29a92920
vxphuc <phuc2652003@gmail.com>
2025-04-10 10:39:23 +0700

    update 10h30

--- src/pages/Adress/index.js
+++ src/pages/Adress/index.js
@@ -19,8 +19,37 @@ function Adress() {
           <span className={`${styles.title__text}`}>Thông tin nhận hàng</span>
         </div>
         <form className={`${styles.form}`}>
-          <div className={`${styles.form__group}`}></div>
+          <div className={`${styles.form__group}`}>
+            <div className={`${styles.info}`}>
+            <input type="radio" name="name" id="A"  className={styles.radio} value=""/>
+            <label for="name">Anh A,</label>
+            <label for="name">09999999</label><br/>
+            </div>
+            <div className={`${styles.change}`}>
+              <button>Sửa</button>
+              <button>Xóa</button>
+            </div>
+            <p className={`${styles.add}`}>nha thk zinh, 11,111</p>
+          </div>
         </form>
+        <form className={`${styles.form}`}>
+          <div className={`${styles.form__group}`}>
+            <div className={`${styles.info}`}>
+            <input type="radio" name="name" id="A"  className={styles.radio} value=""/>
+            <label for="name">Anh A,</label>
+            <label for="name">09999999</label><br/>
+            </div>
+            <div className={`${styles.change}`}>
+              <button>Sửa</button>
+              <button>Xóa</button>
+            </div>
+            <p className={`${styles.add}`}>nha thk zinh, 11,111</p>
+          </div>
+        </form>
+        <a className={`${styles.a}`} href="#">+ nhập địa chỉ khác</a>
+        <div className={`${styles.role}`}>
+          <button>Xác nhận</button>
+        </div>
       </div>
     </div>
   );
