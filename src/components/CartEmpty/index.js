ea9b678a2d0d94fe18450fcf0710a37832b09d23
Phungthevinh <ptvstar2003@gmail.com>
2025-04-10 09:30:12 +0700

    upload

---
+++ src/components/CartEmpty/index.js
@@ -0,0 +1,7 @@
+import styles from './CartsEmpty.module.css';
+
+function CartsEmpty() {
+    return ( <div><div>giỏ hàng trống</div></div> );
+}
+
+export default CartsEmpty;
--- src/pages/Carts/index.js
+++ src/pages/Carts/index.js
@@ -7,6 +7,7 @@ import {
 import { NavLink, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import CartsEmpty from "../../components/CartEmpty";
 
 function Carts() {
   let navigate = useNavigate();
@@ -217,153 +218,155 @@ function Carts() {
 
   return (
     <div className={`container ${styles.container} `}>
-      <div className={`${styles.bg_black_20}`}>
-        <main>
-          <div className={`${styles.carts} container`}>
-            <div className={`${styles.titleCarts}`}>
-              <div className={`${styles.iconTitlecarts}`}>
-                <span onClick={() => navigate(-1)}>
-                  <FontAwesomeIcon icon={faChevronLeft} size="lg" />
-                </span>
+      {
+        (!product || product.length ===0) ? (<div><CartsEmpty></CartsEmpty></div>) : (<div className={`${styles.bg_black_20}`}>
+          <main>
+            <div className={`${styles.carts} container`}>
+              <div className={`${styles.titleCarts}`}>
+                <div className={`${styles.iconTitlecarts}`}>
+                  <span onClick={() => navigate(-1)}>
+                    <FontAwesomeIcon icon={faChevronLeft} size="lg" />
+                  </span>
+                </div>
+                <span>Giỏ hàng</span>
               </div>
-              <span>Giỏ hàng</span>
-            </div>
-            <div className={`${styles.bg_transparent}`}>
-              <div className={`${styles.address}`}>
-                <div className={`${styles.pick_up_store}`}>
-                  <div className={`${styles.chose_address}`}>giao đến</div>
-                  <div className={`${styles.address_user}`}>
-                    <span>
-                      <NavLink to='/gio-hang/cap-nhap-dia-chi'>Đổi</NavLink>
-                    </span>
-                    <div className={`${styles.pb4}`}>
-                      <p>12vdt, Xã Vĩnh Trung, TP. Nha Trang, Khánh Hòa dha</p>
-                      <div className={`${styles.textBasic}`}>
-                        <div className={`${styles.name}`}>Anh Vinh</div>
-                        <div>0911147616</div>
+              <div className={`${styles.bg_transparent}`}>
+                <div className={`${styles.address}`}>
+                  <div className={`${styles.pick_up_store}`}>
+                    <div className={`${styles.chose_address}`}>giao đến</div>
+                    <div className={`${styles.address_user}`}>
+                      <span>
+                        <NavLink to='/gio-hang/cap-nhap-dia-chi'>Đổi</NavLink>
+                      </span>
+                      <div className={`${styles.pb4}`}>
+                        <p>12vdt, Xã Vĩnh Trung, TP. Nha Trang, Khánh Hòa dha</p>
+                        <div className={`${styles.textBasic}`}>
+                          <div className={`${styles.name}`}>Anh Vinh</div>
+                          <div>0911147616</div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-
-            {product.map((item, index) => {
-              const price = Number.parseFloat(
-                item.product.price.$numberDecimal
-              );
-              return (
-                <div key={index}>
-                  <div className={`${styles.listCarts}`}>
-                    <div className={`${styles.nameproduct}`}>
-                      <button
-                        onClick={() => handleDelete(item.product._id)}
-                        className={`${styles.deletebtn}`}
-                      >
-                        x
-                      </button>
-                      <img src={item.product.image[0]} alt="anh1"></img>
-                      <div className={styles.productInfo}>
-                        <p className={styles.productName}>
-                          {item.product.name}
-                        </p>
-                      </div>
-                    </div>
-                    <div className={`${styles.content}`}>
-                      <p>
-                        Giá tiền:{" "}
-                        {(price * item.quantity).toLocaleString("vi-VN", {
-                          style: "currency",
-                          currency: "VND",
-                        })}
-                      </p>
-                      <div className={styles.quantityControl}>
-                        <button onClick={(e) => subtraction(item.product._id, e)} className={`${styles.tru}`}>-</button>
-                        <input
-                          type="number"
-                          value={item.quantity}
-                          min="1"
-                          max="99"
-                        ></input>
-                        <button type="button"
-                          onClick={(e) => handleAddition(item.product._id, e)}
-                          className={`${styles.cong}`}
+  
+              {product.map((item, index) => {
+                const price = Number.parseFloat(
+                  item.product.price.$numberDecimal
+                );
+                return (
+                  <div key={index}>
+                    <div className={`${styles.listCarts}`}>
+                      <div className={`${styles.nameproduct}`}>
+                        <button
+                          onClick={() => handleDelete(item.product._id)}
+                          className={`${styles.deletebtn}`}
                         >
-                          +
+                          x
                         </button>
+                        <img src={item.product.image[0]} alt="anh1"></img>
+                        <div className={styles.productInfo}>
+                          <p className={styles.productName}>
+                            {item.product.name}
+                          </p>
+                        </div>
+                      </div>
+                      <div className={`${styles.content}`}>
+                        <p>
+                          Giá tiền:{" "}
+                          {(price * item.quantity).toLocaleString("vi-VN", {
+                            style: "currency",
+                            currency: "VND",
+                          })}
+                        </p>
+                        <div className={styles.quantityControl}>
+                          <button onClick={(e) => subtraction(item.product._id, e)} className={`${styles.tru}`}>-</button>
+                          <input
+                            type="number"
+                            value={item.quantity}
+                            min="1"
+                            max="99"
+                          ></input>
+                          <button type="button"
+                            onClick={(e) => handleAddition(item.product._id, e)}
+                            className={`${styles.cong}`}
+                          >
+                            +
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-
-            <div className={`${styles.delete}`}>
-              <button onClick={handleDeleteAll}>Xóa tất cả</button>
-            </div>
-            <div className={`${styles.payment}`}>
-              <h3>Thông tin thanh toán</h3>
-              <table className={`${styles.table}`}>
-                <tbody>
-                  <tr>
-                    <td>Tổng tiền</td>
-                    <td>{total}</td>
-                  </tr>
-
-                  <tr>
-                    <td>
-                      <input onClick={handlechecker} type="checkbox"></input>{" "}
-                      {`sử dụng${user.token} điểm`}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Tổng đơn hàng</td>
-                    <td>{totalOrder}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className={`${styles.description}`}>
-              <p>Nhập mô tả đơn hàng</p>
-              <textarea
-                rows="4"
-                cols="77"
-                placeholder="Nhập yêu cầu của bạn (nếu có)"
-              ></textarea>
-            </div>
-
-            <div className={styles.stickyFooter}>
-              <div className={styles.footerActions}>
-
-                {/* xử lý chọn phương thức thanh toán */}
-                <button
-                  className={styles.paybtn}
-                  onClick={() => setShowPaymentMethod(!showPaymentMethod)}
-                >
-                  Đổi hình thức thanh toán
-                </button>
-
-                {showPaymentMethod && (
-                  <div className={styles.paymentPopup}>
-                    <ul>
-                      <li><input type="radio" name="payment" /> Tiền mặt khi nhận hàng</li>
-                      <li><input type="radio" name="payment" /> Thanh toán qua ngân hàng</li>
-                    </ul>
-                  </div>
-                )}
-                {/* kết thúc xử lý */}
-
-                <button className={styles.btn}>
-                  <span className={styles.orderText}>Đặt hàng:</span>
-                  <span className={styles.orderPrice}>{totalOrder}</span>
-                </button>
+                );
+              })}
+  
+              <div className={`${styles.delete}`}>
+                <button onClick={handleDeleteAll}>Xóa tất cả</button>
               </div>
+              <div className={`${styles.payment}`}>
+                <h3>Thông tin thanh toán</h3>
+                <table className={`${styles.table}`}>
+                  <tbody>
+                    <tr>
+                      <td>Tổng tiền</td>
+                      <td>{total}</td>
+                    </tr>
+  
+                    <tr>
+                      <td>
+                        <input onClick={handlechecker} type="checkbox"></input>{" "}
+                        {`sử dụng ${(user.token) ? user.token : 0} điểm`}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Tổng đơn hàng</td>
+                      <td>{totalOrder}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className={`${styles.description}`}>
+                <p>Nhập mô tả đơn hàng</p>
+                <textarea
+                  rows="4"
+                  cols="77"
+                  placeholder="Nhập yêu cầu của bạn (nếu có)"
+                ></textarea>
+              </div>
+  
+              <div className={styles.stickyFooter}>
+                <div className={styles.footerActions}>
+  
+                  {/* xử lý chọn phương thức thanh toán */}
+                  <button
+                    className={styles.paybtn}
+                    onClick={() => setShowPaymentMethod(!showPaymentMethod)}
+                  >
+                    Đổi hình thức thanh toán
+                  </button>
+  
+                  {showPaymentMethod && (
+                    <div className={styles.paymentPopup}>
+                      <ul>
+                        <li><input type="radio" name="payment" /> Tiền mặt khi nhận hàng</li>
+                        <li><input type="radio" name="payment" /> Thanh toán qua ngân hàng</li>
+                      </ul>
+                    </div>
+                  )}
+                  {/* kết thúc xử lý */}
+  
+                  <button className={styles.btn}>
+                    <span className={styles.orderText}>Đặt hàng:</span>
+                    <span className={styles.orderPrice}>{totalOrder}</span>
+                  </button>
+                </div>
+              </div>
+  
+  
             </div>
-
-
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>)
+      }
     </div>
   );
 }
