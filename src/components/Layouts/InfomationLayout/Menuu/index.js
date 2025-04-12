5ece3f45c24411d8ce7046fc8b4d2107113759ed
vxphuc <phuc2652003@gmail.com>
2025-04-12 10:19:09 +0700

    u

--- src/components/Layouts/InfomationLayout/Menuu/index.js
+++ src/components/Layouts/InfomationLayout/Menuu/index.js
@@ -1,12 +1,13 @@
 import styles from './Menuu.module.css';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressBook, faReceipt } from "@fortawesome/free-solid-svg-icons";
 function Menu() {
   return (
     <div>
       {" "}
       <div className={`${styles.infomation}`}>
-        <button className={`${styles.order}`}>Đơn hàng đã mua</button>
-        <button className={`${styles.ifm}`}>Thông tin và địa chỉ</button>
+        <button className={`${styles.order}`}><FontAwesomeIcon icon={faReceipt} /> Đơn hàng đã mua</button>
+        <button className={`${styles.ifm}`}><FontAwesomeIcon icon={faAddressBook} /> Thông tin và địa chỉ</button>
         <button className={`${styles.logout}`}>Đăng xuất</button>
         <button className={`${styles.point}`}>
           Tổng điểm tích lũy: 1000000
