6b916725d7559372476652aa03292551b1a4daf9
Phungthevinh <ptvstar2003@gmail.com>
2025-04-12 15:06:22 +0700

    Update index.js

--- src/pages/Bill/index.js
+++ src/pages/Bill/index.js
@@ -23,7 +23,7 @@ function Bill() {
 
   useEffect(() => {
     const fetchBills = async () => {
-      const response = await axios.get("http://localhost:5000/bill/user", {
+      const response = await axios.get("https://web-dt.onrender.com/bill/user", {
         headers: {
           Authorization: `Bearer ${Token}`,
         },
