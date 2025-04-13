fe2646e545167637c57fb22844442a722953d81a
vxphuc <phuc2652003@gmail.com>
2025-04-13 18:22:31 +0700

    update cn

--- src/pages/PayMentBank/index.js
+++ src/pages/PayMentBank/index.js
@@ -16,7 +16,7 @@ function PayMentBank() {
                     <p>Chờ xác nhận thanh toán <strong>Chuyển Khoản</strong></p>
                 </div>
                 <div>
-                    <p>Đơn này sẽ được giao cho anh zinh và thanh toán tiền mặt cho đến khi chuyển khoản thành công</p>
+                    <p>Đơn này sẽ được giao cho anh zinh và thanh toán tiền mặt</p>
                 </div>
             </button>
         </div>
@@ -48,9 +48,40 @@ function PayMentBank() {
                 <p className={`${styles.contact}`}>Cần hỗ trợ vui lòng liên hệ: <a href="#">099 899</a></p>
             </div>
         </div>
-        <div>
-            
-        </div>
+            <div className={`${styles.qrcode}`}>
+                <img src="https://res.cloudinary.com/dlqxlgre4/image/upload/v1743492660/products/q4fn4lztuuvqowz01csu.webp"></img>
+            </div>
+            <div className={`${styles.ifm}`}>
+            <div className={`${styles.paymentInfo}`}>
+                <table>
+                    <tr>
+                        <td>Ngân hàng:</td>
+                        <td className={`${styles.tdin}`}>Viettinbank</td>
+                    </tr>
+                    <tr>
+                        <td>Số tài khoản:</td>
+                        <td className={`${styles.tdin}`}>09999999999 <button>sao chép</button></td>
+                    </tr>
+                    <tr>
+                        <td>Người hưởng thụ:</td>
+                        <td className={`${styles.tdin}`}>Zinh cắn ác</td>
+                    </tr>
+                    <tr>
+                        <td>Số tiền:</td>
+                        <td className={`${styles.tdin}`}>1.000.000.000đ <button>sao chép</button></td>
+                    </tr>
+                    <tr>
+                        <td>Nội dung:</td>
+                        <td className={`${styles.tdin}`}>zinh quá tệ</td>
+                    </tr>
+                </table>
+                <p className={`${styles.contact}`}>Cần hỗ trợ vui lòng liên hệ: <a href="#">099 899</a></p>
+            </div>
+            </div>
+            <div className={`${styles.detail}`}>
+                <button className={`${styles.home}`}>Trang chủ</button>
+                <button className={`${styles.defaul}`}>Xem đơn hàng</button>
+            </div>
       </div>
     </div>
   );
