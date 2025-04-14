9ab0fd168ce85acc997f28812b9cb8e0efea65ca
vxphuc <phuc2652003@gmail.com>
2025-04-14 16:16:30 +0700

    update 16h15

--- src/components/Layouts/Search/index.js
+++ src/components/Layouts/Search/index.js
@@ -2,6 +2,7 @@ import { TextField, InputAdornment } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import style from "./Search.module.css";
+import { NavLink } from "react-router-dom";
 
 function Search({ cartCount }) {
   return (
@@ -10,12 +11,16 @@ function Search({ cartCount }) {
         <input placeholder="Tìm kiếm..." />
 
         {/* Giỏ hàng chèn vào trong ô tìm kiếm */}
+        <NavLink
+        to="/gio-hang">
+                
         <div className={style.cartInside}>
           <FontAwesomeIcon icon={faCartShopping} className={style.cartIcon} />
           {cartCount > 0 && (
             <span className={style.cartBadge}>{cartCount}</span>
           )}
         </div>
+        </NavLink>  
       </div>
     </div>
   );
