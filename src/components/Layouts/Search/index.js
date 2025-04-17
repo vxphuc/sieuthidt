3cf21078754be7f1e9f134a04ab9c7ffbf922828
vxphuc <phuc2652003@gmail.com>
2025-04-17 12:31:16 +0700

    update 12h30

--- src/components/Layouts/Search/index.js
+++ src/components/Layouts/Search/index.js
@@ -4,13 +4,15 @@ import { faSearch, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import style from "./Search.module.css";
 import { NavLink } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef  } from "react";
 import axios from "axios";
 
 function Search({ onChange, cartCount, searchValue }) {
   const [input, setInput] = useState(searchValue || "");
   const navigate = useNavigate();
   const [suggestions, setSuggestions] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const popUpRef = useRef(null);
 
   useEffect(() => {
     const deylayTimeOut = setTimeout(async () => {
@@ -21,17 +23,37 @@ function Search({ onChange, cartCount, searchValue }) {
               input
             )}`
           );
-          setSuggestions(res.data);
+          if (res.data.length > 0) {
+            setSuggestions(res.data);
+            setIsOpen(true); // mở popup
+          } else {
+            setSuggestions([]);
+            setIsOpen(false); // không có kết quả thì ẩn
+          }
         } catch (error) {
           console.log(error);
         }
+      } else {
+        setSuggestions([]);
+        setIsOpen(false); // rỗng input thì ẩn popup luôn
       }
     }, 500);
 
     return () => clearTimeout(deylayTimeOut);
   }, [input]);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popUpRef.current && !popUpRef.current.contains(event.target)) {
+        setSuggestions([]); // ẩn gợi ý
+      }
+    };
 
-  console.log(suggestions)
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  console.log(suggestions);
 
   return (
     <div className={style.searchContainer}>
@@ -52,13 +74,32 @@ function Search({ onChange, cartCount, searchValue }) {
           </div>
         </NavLink>
       </div>
-      <div className={style.popUpHeader}>
-      <p>sản phẩm gợi ý</p>
+      {isOpen && suggestions.length > 0 && (
+      <div className={style.popUpHeader} ref={popUpRef}>
+        <p>sản phẩm gợi ý</p>
         {suggestions.map((item, index) => {
-          return (<div className={style.Product}>
-          </div>);
+          const giatien = Number.parseInt(item.price.$numberDecimal);
+          const x = giatien.toLocaleString("vi", { style: "currency", currency: "VND" });
+          console.log(x);
+  
+          return (
+            <div key={index} className={style.Product}>
+              <div className={`${style.listProduct}`}>
+                <a className={style.imgProduct} href="#">
+                  <div className={style.listImg}>
+                    <img src={item.image[0]}></img>
+                  </div>
+                  <div className={style.content}>
+                    <h3>{(item.name.length > 30)? item.name.slice(0, 30) + `...` : item.name}</h3>
+                    <strong>{x}</strong>
+                  </div>
+                </a>
+              </div>
+            </div>
+          );
         })}
       </div>
+       )}
     </div>
   );
 }
