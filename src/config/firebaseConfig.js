e4360e1f40d1f104a0bc5310c75a54a32c65cf0d
Phungthevinh <ptvstar2003@gmail.com>
2025-04-18 09:57:51 +0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDE29H2f2Eo3FqPCi2QD_7kOZz0ab7JUt4",
-    authDomain: "dtweb-7cfee.firebaseapp.com",
-    projectId: "dtweb-7cfee",
-    storageBucket: "dtweb-7cfee.firebasestorage.app",
-    messagingSenderId: "558236469233",
-    appId: "1:558236469233:web:69fd6f464334d19e0fe650",
-    measurementId: "G-MWEDEZG6X8"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCK1MdtjrczBJZbwde8n4NtdYh1h7a7u_w",
+  authDomain: "dtshop-50141.firebaseapp.com",
+  projectId: "dtshop-50141",
+  storageBucket: "dtshop-50141.firebasestorage.app",
+  messagingSenderId: "25769242398",
+  appId: "1:25769242398:web:3588c0392fcbb53780a30f",
+  measurementId: "G-5X8K17NRP7",
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
