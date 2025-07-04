1a37875ef5716a600a08732f88cfbde29c7cb5e9
Phungthevinh <ptvstar2003@gmail.com>
2025-07-04 08:25:12 +0700

    up

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef, useContext } from "react";
-import axios from "axios";
 import styles from "./Home.module.css";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -16,6 +15,7 @@ import ProductHome from "../ProductHome";
 import { CartContext } from "../../contexts/CartContext";
 import BackgroundPopup from "../../components/BackgroundPopup";
 import { io } from "socket.io-client";
+import api from "../../api/axios";
 
 function Home() {
   const navigate = useNavigate();
@@ -39,9 +39,9 @@ function Home() {
   };
 
   const confirmAddToCart = () => {
-    axios
+    api
       .post(
-        "https://dtweb.onrender.com/cart/create",
+        "/cart/create",
         {
           productID: popupProduct._id,
           quantity: quantity,
@@ -57,16 +57,16 @@ function Home() {
 
   //new product
   useEffect(() => {
-    axios
-      .get("https://dtweb.onrender.com/product/newProduct")
+    api
+      .get("/product/newProduct")
       .then((res) => setNewProduct(res.data))
       .catch((error) => console.log(error));
   }, []);
 
   //banner
   useEffect(() => {
-    axios
-      .get("https://dtweb.onrender.com/sign-in/banner")
+    api
+      .get("/sign-in/banner")
       .then((res) => setImg(res.data))
       .catch((error) => console.log(error));
   }, []);
