312d478242eae25ff1e15a84192fc8f0d48f6ac8
Phungthevinh <ptvstar2003@gmail.com>
2025-07-04 10:06:40 +0700

    Update index.js

--- src/components/Layouts/HeaderOnly/Header/index.js
+++ src/components/Layouts/HeaderOnly/Header/index.js
@@ -1,18 +1,14 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink,useNavigate } from "react-router-dom";
 import style from "./Header.module.css";
 import Auth from "../../../Auth";
 import Search from "../../Search";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCartShopping,
-  faBars,
-  faBell,
-  faRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faBars, faBell, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect, useRef, useContext } from "react";
 import axios from "axios";
 import { CartContext } from "../../../../contexts/CartContext";
 import { io } from "socket.io-client";
+import api from "../../../../api/axios"
 
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
@@ -22,16 +18,11 @@ function Header() {
   const { cartCount } = useContext(CartContext);
   const [notifications, setNotifications] = useState([]);
   const [popupnotifications, setpopupNotifications] = useState(false);
-
+  
   const navigate = useNavigate();
   const handleLogout = async () => {
-    try {
-      await axios.post("https://dtweb.onrender.com/sign-in/logout", {}, { withCredentials: true });
-      window.location.href = "/"; // chuyển về trang chủ
-      window.location.reload();   // ép reload lại toàn bộ app
-    } catch (err) {
-      console.error("Logout failed", err);
-    }
+    localStorage.removeItem('authToken');
+    window.location.reload()
   };
   // SOCKET: dùng ref để giữ instance duy nhất
   const socketRef = useRef(null);
@@ -69,9 +60,8 @@ function Header() {
   useEffect(() => {
     const fetchNotifications = async () => {
       try {
-        const res = await axios.get(
-          "https://dtweb.onrender.com/sign-in/NotificationAdmin",
-          { withCredentials: true }
+        const res = await api.get(
+          "/sign-in/NotificationAdmin"
         );
         setNotifications(res.data);
       } catch (err) {
@@ -98,9 +88,8 @@ function Header() {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await axios.get(
-          "https://dtweb.onrender.com/sign-in/user-profile",
-          { withCredentials: true }
+        const res = await api.get(
+          "/sign-in/user-profile"
         );
         setUserRole(res.data.role);
       } catch (err) {
@@ -164,10 +153,7 @@ function Header() {
                 </NavLink>
               </li>
               <li>
-                <button
-                  className={style.navItemButton}
-                  onClick={() => setpopupNotifications((prev) => !prev)}
-                >
+                <button className={style.navItemButton} onClick={() => setpopupNotifications((prev) => !prev)}>
                   <FontAwesomeIcon icon={faBell} className={style.cartIcon} />
                 </button>
               </li>
@@ -177,10 +163,7 @@ function Header() {
               {userRole && (
                 <li>
                   <button className={style.logoutButton} onClick={handleLogout}>
-                    <FontAwesomeIcon
-                      icon={faRightFromBracket}
-                      className={style.cartIcon}
-                    />
+                    <FontAwesomeIcon icon={faRightFromBracket} className={style.cartIcon} />
                   </button>
                 </li>
               )}
@@ -206,10 +189,7 @@ function Header() {
             ) : (
               <ul className={style.notificationList}>
                 {notifications.map((noti, index) => (
-                  <NavLink
-                    to={`/quan-tri/chi-tiet/${noti.orderId}`}
-                    key={index}
-                  >
+                  <NavLink to={`/quan-tri/chi-tiet/${noti.orderId}`} key={index}>
                     <li className={style.notificationItem}>{noti.message}</li>
                   </NavLink>
                 ))}
