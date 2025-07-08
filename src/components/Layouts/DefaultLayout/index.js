28e65de167b35270be3ec58a991091fa968b8ad8
Phungthevinh <ptvstar2003@gmail.com>
2025-07-08 08:42:20 +0700

    up

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -3,7 +3,6 @@ import Footer from "./Footer";
 import TypeProduct from "./TypeProduct";
 import styles from "./DefaultLayout.module.css";
 
-
 function DefaultLayout({ children }) {
   return (
     <div className="">
