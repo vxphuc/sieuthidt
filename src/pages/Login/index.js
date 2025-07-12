cb5eca2980df31dc474f69a5d73dcfb5d5a56ff6
Phungthevinh <ptvstar2003@gmail.com>
2025-07-12 09:36:33 +0700

    up

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -101,7 +101,6 @@ function Login() {
 
       // Gửi token lên backend
       const response = await api.post("/sign-in", { idToken, numberPhone: phone });
-      console.log(phone)
       // console.log("Đăng nhập thành công:", response.data);
       // Lưu dữ liệu người dùng và token vào localStorage theo đúng format backend trả về
       localStorage.setItem("authToken", idToken);
