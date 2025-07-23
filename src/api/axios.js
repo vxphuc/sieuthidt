8ea6205862630fad816d5c106121bed8599de120
Phungthevinh <ptvstar2003@gmail.com>
2025-07-23 11:03:03 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://dtweb.onrender.com',
   timeout: 10000,
 });
 
