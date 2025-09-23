13547e83ab94d8c9944f2f25567ff9524a5866b1
vxphuc <phuc2652003@gmail.com>
2025-09-23 16:17:54 +0700

    update

---
+++ src/pages/Admin/luckywheel/config.js
@@ -0,0 +1,6 @@
+const API_BASE =
+  (typeof import.meta !== "undefined" && import.meta.env?.VITE_API_BASE) ||
+  (typeof process !== "undefined" && process.env?.REACT_APP_API_BASE) ||
+  "http://localhost:5000";
+
+export default API_BASE;
