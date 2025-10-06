174b8eca0d0929318df30158058a31848052f8b8
vxphuc <phuc2652003@gmail.com>
2025-10-06 10:08:15 +0700

    Update config.js

--- src/pages/Admin/luckywheel/config.js
+++ src/pages/Admin/luckywheel/config.js
@@ -1,6 +1,6 @@
 const API_BASE =
   (typeof import.meta !== "undefined" && import.meta.env?.VITE_API_BASE) ||
   (typeof process !== "undefined" && process.env?.REACT_APP_API_BASE) ||
-  "http://localhost:5000";
+  "https://luckywheel-backend-acgn.onrender.com";
 
 export default API_BASE;
