51b7031316c0f9d0ae01840ea2e01c07a99a3d4d
Phungthevinh <ptvstar2003@gmail.com>
2026-01-12 08:49:20 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< Updated upstream
   baseURL: process.env.REACT_APP_SIEU_THI_API,
-=======
-  baseURL: "https://sieuthidt.io.vn" || 'https://sieuthidt.io.vn',
->>>>>>> Stashed changes
   timeout: 10000,
 });
 
